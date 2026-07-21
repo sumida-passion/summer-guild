d58bee27741ef8ce1c0ee01177a7a2f50c0d93be
sumida-passion <tsumida@nara-su.ac.jp>
2026-07-21 15:43:01 +0900

    Add files via upload

---
+++ data/items/items.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/* =========================================================
+   ギルドショップ商品マスター
+
+   新しい商品は、この配列へ1件追加する。
+   idは重複させない。
+   ========================================================= */
+
+window.GUILD_SHOP_ITEMS = [
+
+    {
+        id: "green_tshirt",
+        name: "森風の緑Tシャツ",
+        price: 20
+    },
+
+    {
+        id: "lightning_tshirt",
+        name: "稲妻のTシャツ",
+        price: 30
+    },
+
+    {
+        id: "doctor_white_coat",
+        name: "癒やし手の白衣",
+        price: 80
+    },
+
+    {
+        id: "black_dog_costume",
+        name: "黒い犬の着ぐるみ",
+        price: 120
+    },
+
+    {
+        id: "kyudo_uniform",
+        name: "静心の弓道着",
+        price: 180
+    },
+
+    {
+        id: "barbarian_outfit",
+        name: "荒野の戦士装備",
+        price: 300
+    }
+
+];
