3009b1af5448712c1895d8f96c68a2d2dd5b4980
sumida-passion <tsumida@nara-su.ac.jp>
2026-07-22 10:53:13 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@
    ・LocalStorage（GP・衣装・招待済み楽団など）には触れない
    ========================================================= */
 
-const SW_VERSION = "2026.07.22-1";
+const SW_VERSION = "2026.07.22-2";
 const CACHE_PREFIX = "summer-guild-";
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${SW_VERSION}`;
 
